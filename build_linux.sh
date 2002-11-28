f02b31da5971f826ed8b3890e9b2bb23e4a562bd
zavatt <zavatt>
2002-11-28 14:33:26 +0000

    build system changed to cross-compile for windows

---
+++ build_linux.sh
@@ -0,0 +1,3 @@
+cp Makefile.Linux Makefile
+cp automa/Makefile.Linux automa/Makefile
+cp menu/Makefile.Linux menu/Makefile
---
+++ build_win32.sh
@@ -0,0 +1,3 @@
+cp Makefile.cross.w32 Makefile
+cp automa/Makefile.cross.w32 automa/Makefile
+cp menu/Makefile.cross.w32 menu/Makefile
