82bd6f9e0849c182793921b2f02903185681c570
zavatt <zavatt>
2003-01-24 11:35:27 +0000

    *** empty log message ***

---
+++ menu/MenuItemBigBackground.h
@@ -0,0 +1,50 @@
+/* -*- C++ -*- */
+/*
+  GAV - Gpl Arcade Volleyball
+  
+  Copyright (C) 2002
+  GAV team (http://sourceforge.net/projects/gav/)
+
+  This program is free software; you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation; either version 2 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with this program; if not, write to the Free Software
+  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
+*/
+
+#ifndef __MENUITEMBIGBACKGROUND_H__
+#define __MENUITEMBIGBACKGROUND_H__
+
+#include <SDL.h>
+#include "MenuItem.h"
+#include "globals.h"
+
+class MenuItemBigBackground: public MenuItem {
+  bool isBig;
+  const char * currThemeName;
+
+public:
+  MenuItemBigBackground() {
+    isBig = false;
+    label = std::string("Big Background: No");
+  }
+
+  int execute(std::stack<Menu *> &s) {
+    isBig = !isBig;
+    label = std::string(isBig?"Big Background: Yes":"Big Background: No");
+    currThemeName = CurrentTheme->name();
+    delete(CurrentTheme);
+    CurrentTheme = new Theme(currThemeName, isBig);
+    return(0);
+  }
+};
+
+#endif
