eb9c51f2e1da036f9631f59414eeacd8a5193b1b
zavatt <zavatt>
2005-12-09 11:32:18 +0000

    different keyboard handling for menu, patch from Kam

--- menu/Menu.cpp
+++ menu/Menu.cpp
@@ -1,32 +1,12 @@
-/* -*- C++ -*- */
-/*
-  GAV - Gpl Arcade Volleyball
-  
-  Copyright (C) 2002
-  GAV team (http://sourceforge.net/projects/gav/)
-
-  This program is free software; you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
-  the Free Software Foundation; either version 2 of the License, or
-  (at your option) any later version.
-
-  This program is distributed in the hope that it will be useful,
-  but WITHOUT ANY WARRANTY; without even the implied warranty of
-  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
-
-  You should have received a copy of the GNU General Public License
-  along with this program; if not, write to the Free Software
-  Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
-*/
-
 #include "Menu.h"
 #include <string.h>
 #include "SoundMgr.h"
 
 using namespace std;
 
-void Menu::add(MenuItem * mi) {
+
+void Menu::add(MenuItem * mi)
+{
   if (currentItem < 0)
     currentItem = 0;
   if ((mi->getLabel()).length() > maxLabelLength)
@@ -34,26 +14,55 @@ void Menu::add(MenuItem * mi) {
   items.push_back(mi);
 }
 
-int Menu::execute(InputState *is, std::stack<Menu *> &s) {
+
+int Menu::execute(InputState *is, std::stack<Menu *> &s)
+{
   SDL_Rect rect;
   const char * label;
   ScreenFont * font;
   static bool spacePressed = false;
+  static bool PrevUp = false;
+  static bool PrevDown = false;
 
-  if ( is->getKeyState()[SDLK_UP] && (currentItem > 0) ) {
-    currentItem--;
+  // Up  
+  if (is->getKeyState()[SDLK_UP]) {
+    if (!PrevUp) {
+      if (currentItem > 0) {
+        currentItem--;
 #ifdef AUDIO
-    soundMgr->playSound(SND_MENU_SELECT);
-#endif // AUDIO
-  }
-  if ( is->getKeyState()[SDLK_DOWN] &&
-       (currentItem < (int) (items.size() - 1)) ) {
-    currentItem++;
+        soundMgr->playSound (SND_MENU_SELECT);
+#endif
+      } else {
+        currentItem=(int)(items.size()-1);
 #ifdef AUDIO
-    soundMgr->playSound(SND_MENU_SELECT);
-#endif // AUDIO
-  }
-  /* draw menu items labels */
+        soundMgr->playSound (SND_MENU_SELECT);
+#endif
+      }
+      PrevUp = true;
+    }
+  } else
+    PrevUp =false;
+
+  // Down
+  if (is->getKeyState()[SDLK_DOWN]) {
+    if (!PrevDown) {
+      if (currentItem < (int)(items.size()-1)) {
+        currentItem++;
+#ifdef AUDIO
+        soundMgr->playSound (SND_MENU_SELECT);
+#endif
+      } else {
+        currentItem=0;
+#ifdef AUDIO
+        soundMgr->playSound (SND_MENU_SELECT);
+#endif
+      }
+      PrevDown = true;
+    }
+  } else
+    PrevDown = false;
+
+  // draw menu items labels
   rect.y = 30;
   for ( unsigned int it = 0; it < items.size(); it++ ) {
     label = items[it]->getLabel().c_str();
@@ -65,8 +74,8 @@ int Menu::execute(InputState *is, std::stack<Menu *> &s) {
     font->printXY(screen, &rect, items[it]->getLabel().c_str());
     rect.y += font->charHeight();
   }
-
-  /* call the execute method of the current item, if an event occurred */
+  
+  // call the execute method of the current item, if an event occurred
   if ( (is->getKeyState()[SDLK_SPACE]) ||
        (is->getKeyState()[SDLK_RETURN]) ) {
     spacePressed = true;
